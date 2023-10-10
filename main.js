134445a9db8297b2ff36a97d331d6e2950a6537f
tomyvdr <tomyvet1496@gmail.com>
2023-10-10 15:11:16 -0300

    Proyecto finalizado

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
 })
+
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 400 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 400 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 400 });
